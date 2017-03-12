361e8c410e8af7edded4214ea823e562a9fa6a6c
Luke Cavanagh <lukecav@users.noreply.github.com>
2017-03-11 19:42:03 -0700

    Create image-optin.php

---
+++ image-optin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Imageoptin image optimization
+ * Plugin URI: https://github.com/jonathanbossenger/imageoptim
+ * Description: A set of WP-CLI commands to support the image optimization of site images using jpegoptim and optinpng
+ * Version: 1.0
+ * Author: Jonathan Bossenger and Luke Cavanagh
+ * Author URI: https://github.com/jonathanbossenger/imageoptim
+ * Text Domain: image-optimization
+ * Domain Path: /languages
+ *
+ * @package imageoptin\image_optimization
+ */
