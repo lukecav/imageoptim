9a941fcd92dbbc8e0c5554dfc7dc206c313d4ac4
Luke Cavanagh <lukecav@users.noreply.github.com>
2017-03-13 11:17:10 -0600

    Create image-optim.php

---
+++ image-optim.php
@@ -0,0 +1,23 @@
+
+<?php
+/*
+Plugin Name: wp-usergen-cli
+Plugin URI:  https://github.com/jonathanbossenger/imageoptim
+Description: A WP-CLI addon command that generates random users. Useful for testing purposes.
+Version: 1.0.0
+Author:      Jonathan Bossenger and Luke Cavanagh
+Author URI:  https://github.com/jonathanbossenger/imageoptim
+License:     GPLv2+
+*/
+/**
+ * Load extension if WP CLI exists.
+ *
+ * @return void
+ * @since 1.0.0
+ */
+ function wp_imageoptim_cli_load() {
+	if ( defined( 'WP_CLI' ) && WP_CLI ) {
+		include dirname(__FILE__) . '/cli.php';
+	}
+}
+add_action( 'plugins_loaded', 'wp_imageoptim_cli_load' );
